f88e7d5d16758a0f723121f71408a7ec2cc6c255
Samk13 <samarbid13@gmail.com>
2021-01-10 22:59:15 +0100

    update scss styles

--- src/pages/components/headerApp.js
+++ src/pages/components/headerApp.js
@@ -4,16 +4,14 @@ import headerStyles from './headerApp.module.scss'
 
 const HeaderApp = () => {
   return (
-    <header className={headerStyles.header}>
-      <h1>
-        <Link to="/">Sam Arbid</Link>
-      </h1>
-      <nav>
+    <header>
+      <nav className={headerStyles.navContainer}>
+        <h1>
+          <Link to="/">Sam Arbid</Link>
+        </h1>
         <ul>
           <li>
-            <Link className={headerStyles.link} to="/contact">
-              Contact
-            </Link>
+            <Link to="/contact">Contact</Link>
           </li>
           <li>
             <Link to="/about">about</Link>
