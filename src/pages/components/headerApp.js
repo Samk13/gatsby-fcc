f36909395dd4e1e2c75b50cbd087ba2cf2e99571
Samk13 <samarbid13@gmail.com>
2021-01-11 01:22:11 +0100

    update header styles

--- src/pages/components/headerApp.js
+++ src/pages/components/headerApp.js
@@ -7,19 +7,27 @@ const HeaderApp = () => {
     <header>
       <nav className={headerStyles.navContainer}>
         <h1>
-          <Link to="/">Sam Arbid</Link>
+          <Link to="/" activeClassName={headerStyles.activeNavItem}>
+            Sam Arbid
+          </Link>
         </h1>
         <ul>
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link to="/contact" activeClassName={headerStyles.activeNavItem}>
+              Contact
+            </Link>
           </li>
           <li>
-            <Link to="/about">about</Link>
+            <Link to="/about" activeClassName={headerStyles.activeNavItem}>
+              about
+            </Link>
           </li>
         </ul>
       </nav>
       <section>
-        <Link to="/">Go Back</Link>
+        <Link to="/" activeClassName={headerStyles.activeClassName}>
+          Go Back
+        </Link>
       </section>
     </header>
   )
